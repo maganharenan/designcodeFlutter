d2860e9ec89d4fb2cbd972288bf13aa0ea851fff
Renan Maganha <renan.maganha@mc1.com.br>
2022-03-30 06:48:28 -0300

    Finished profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,20 @@
 import 'dart:io';
+import 'package:designcode/components/cards/completed_courses_card.dart';
+import 'package:designcode/components/lists/certificate_viewer.dart';
+import 'package:designcode/components/lists/completed_courses_list.dart';
 import 'package:designcode/constants.dart';
+import 'package:designcode/model/course.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+
+  final List<String> badges = [
+    'badge-01.png',
+    'badge-02.png',
+    'badge-03.png',
+    'badge-04.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -79,10 +89,140 @@ class ProfileScreen extends StatelessWidget {
                         ],
                       ),
                     ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(6.0),
+                              child: Container(
+                                padding: EdgeInsets.all(6.0),
+                                child: CircleAvatar(
+                                  backgroundImage: AssetImage('asset/images/profile.png'),
+                                  radius: 30.0,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: kBackgroundColor,
+                                  borderRadius: BorderRadius.circular(42.0),
+                                ),
+                              ),
+                            ),
+                            height: 84.0,
+                            width: 84.0,
+                            decoration: BoxDecoration(
+                              gradient: RadialGradient(
+                                colors: [
+                                  Color(0xFF00AEFF),
+                                  Color(0xFF0076FF),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.circular(42.0)
+                            ),
+                          ),
+                          SizedBox(width: 16.0,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Renan Maganha", style: kTitle2Style,),
+                              SizedBox(height: 8.0,),
+                              Text("iOS Developer", style: kSecondaryCalloutLabelStyle,),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 28.0, bottom: 16.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("Badges", style: kHeadlineLabelStyle,),
+                                Row(
+                                  children: [
+                                    Text("See all", style: kSearchPlaceholderStyle,),
+                                    Icon(Icons.chevron_right, color: kSecondaryLabelColor),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 16.0,),
+                          Container(
+                            height: 112.0,
+                            child: ListView.builder(
+                              itemBuilder: (context, index){
+                                return Container(
+                                  padding: EdgeInsets.only(left: 20.0, right: index != 3 ? 0.0 : 20.0),
+                                  decoration: BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: kShadowColor.withOpacity(0.1),
+                                        offset: Offset(0,12),
+                                        blurRadius: 18.0,
+                                      ),
+                                    ],
+                                  ),
+                                  child: Image.asset('asset/badges/${badges[index]}'),
+                                );
+                              },
+                              padding: EdgeInsets.only(bottom: 28.0),
+                              scrollDirection: Axis.horizontal,
+                              itemCount: badges.length,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 32.0, left: 20.0, right: 20.0, bottom: 12.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Certificates", style: kHeadlineLabelStyle,),
+                      Row(
+                        children: [
+                          Text("See all", style: kSearchPlaceholderStyle,),
+                          Icon(Icons.chevron_right, color: kSecondaryLabelColor,),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            CertificateViewer(),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 12.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Completed courses", style: kHeadlineLabelStyle,),
+                      Row(
+                        children: [
+                          Text("See all", style: kSearchPlaceholderStyle,),
+                          Icon(Icons.chevron_right, color: kSecondaryLabelColor,),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            CompletedCoursesList(),
+            SizedBox(height: 28.0,),
           ],
         ),
       ),
